a3936c9420f450ce9792db745b25139737f86823
Sergio Clebal <sergioclebal@gmail.com>
2022-06-07 20:25:56 +0200

    Implement missing decode_coord. Implement layer search. Test code with lines and polygons

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::{fs, process::exit};
 
-const X: u8 = 4;
-const Y: u8 = 2;
-const Z: u8 = 6;
+// const X: u8 = 4;
+// const Y: u8 = 2;
+// const Z: u8 = 6;
+const X: u8 = 150;
+const Y: u8 = 194;
+const Z: u8 = 9;
+// const X: u8 = 0;
+// const Y: u8 = 2;
+// const Z: u8 = 3;
+// const LAYER: &str = "layer0";
+const LAYER: &str = "admin";
+// const LAYER: &str = "default";
 
 mod vector_tile;
 
@@ -17,14 +26,27 @@ type Coord = [f64; 2];
 fn main() {
     // let feature_collection: Vec<Point> = Vec::new();
 
-    let file_name = format!("src/points_{}-{}-{}.mvt", X, Y, Z);
+    let file_name = format!("src/{}-{}-{}.mvt", X, Y, Z);
 
     let contents = fs::read(file_name).expect("Something went wrong reading the file");
     let tile = Tile::parse_from_bytes(&contents).expect("Something went wrong parsing the file");
 
-    let layer = &tile.layers[0];
-    // layer.extent.expect("Layer must provider an extent");
-    let extent = 4096_u32;
+    // Available layers
+    // println!(
+    //     "{:?}",
+    //     tile.layers
+    //         .iter()
+    //         .map(|layer| layer.name.as_ref().unwrap())
+    //         .collect::<Vec<&String>>()
+    // );
+
+    let layer = tile
+        .layers
+        .iter()
+        .find(|layer| layer.name.as_ref().unwrap() == LAYER)
+        .expect("Layer not found");
+
+    let extent = layer.extent.expect("Layer must provider an extent");
 
     let size = extent * 2_u32.pow(Z as u32);
     let x0 = extent * X as u32;
@@ -47,23 +69,8 @@ fn main() {
         new_line
     };
 
-    let local_project = |line: &Vec<Coord>| -> Vec<Coord> {
-        let mut new_line: Vec<Coord> = Vec::with_capacity(line.len());
-        for item in line.iter() {
-            let mut p: Coord = [0_f64; 2];
-
-            p[0] = item[0] / extent as f64;
-            p[1] = item[1] / extent as f64;
-
-            new_line.push(p);
-        }
-
-        new_line
-    };
-
     for item in layer.features.iter() {
         to_geojson(item, project);
-        to_geojson(item, local_project);
     }
 }
 
@@ -80,18 +87,28 @@ fn to_geojson(feature: &Feature, project: impl Fn(&Vec<Coord>) -> Vec<Coord>) {
     match feature_type {
         GeomType::POINT => {
             let coordinates = project(&geometry[0]);
-            println!("{:?}", coordinates);
+            println!("Point {:?}", coordinates);
         }
         GeomType::UNKNOWN => todo!(),
-        GeomType::LINESTRING => todo!(),
-        GeomType::POLYGON => todo!(),
+        GeomType::LINESTRING => {
+            let coordinates = project(&geometry[0]);
+            println!("Line {:?}", coordinates);
+        }
+        GeomType::POLYGON => {
+            let coordinates = project(&geometry[0]);
+            println!("Polygon {:?}", coordinates);
+        }
     }
+}
+
+fn decode_coord(coord: i32) -> i32 {
+    (coord >> 1) ^ (-(coord & 1))
 }
 
 fn load_geometry(geom: &Vec<u32>) -> Vec<Vec<Coord>> {
     let mut idx = 0_usize;
 
-    let mut length = 0_i128;
+    let mut length = 0_usize;
 
     let mut cmd = 1_u8;
     let mut x = 0_f64;
@@ -100,14 +117,10 @@ fn load_geometry(geom: &Vec<u32>) -> Vec<Vec<Coord>> {
     let mut lines: Vec<Vec<Coord>> = Vec::new();
     let mut line: Vec<Coord> = Vec::new();
 
-    loop {
-        if idx >= geom.len() {
-            break;
-        }
-
+    while idx < geom.len() {
         if length == 0 {
             cmd = (geom[idx] & 0x7) as u8;
-            length = (geom[idx] >> 3) as i128;
+            length = (geom[idx] >> 3) as usize;
 
             idx += 1;
         }
@@ -115,8 +128,8 @@ fn load_geometry(geom: &Vec<u32>) -> Vec<Vec<Coord>> {
         length -= 1;
 
         if cmd == 1 || cmd == 2 {
-            x += geom[idx] as f64;
-            y += geom[idx + 1] as f64;
+            x += decode_coord(geom[idx] as i32) as f64;
+            y += decode_coord(geom[idx + 1] as i32) as f64;
 
             idx += 2;
 
@@ -128,9 +141,7 @@ fn load_geometry(geom: &Vec<u32>) -> Vec<Vec<Coord>> {
                 line = Vec::new();
             }
 
-            if line.len() == 0 {
-                line.push([x, y]);
-            }
+            line.push([x, y]);
         } else if cmd == 7 {
             if line.len() > 0 {
                 line.push(line[0].clone()); // closePolygon
@@ -146,3 +157,17 @@ fn load_geometry(geom: &Vec<u32>) -> Vec<Vec<Coord>> {
 
     lines
 }
+
+// let local_project = |line: &Vec<Coord>| -> Vec<Coord> {
+//     let mut new_line: Vec<Coord> = Vec::with_capacity(line.len());
+//     for item in line.iter() {
+//         let mut p: Coord = [0_f64; 2];
+
+//         p[0] = item[0] / extent as f64;
+//         p[1] = item[1] / extent as f64;
+
+//         new_line.push(p);
+//     }
+
+//     new_line
+// };
